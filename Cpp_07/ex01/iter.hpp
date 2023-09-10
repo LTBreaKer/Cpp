df0b06f52b9fa54f9f69b524dcb67e1bb5d007d3
LTBreaKer <50266951+LTBreaKer@users.noreply.github.com>
2023-09-10 18:52:33 +0000

    Cpp_07_ex01

--- Cpp_07/ex01/iter.hpp
+++ Cpp_07/ex01/iter.hpp
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aharrass <aharrass@student.1337.ma >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/10 14:46:21 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/10 15:16:31 by aharrass         ###   ########.fr       */
+/*   Created: 2023/09/10 15:22:53 by aharrass          #+#    #+#             */
+/*   Updated: 2023/09/10 15:51:02 by aharrass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
-template<typename T>
-void	swap(T &a, T &b)	{
-	T tmp = a;
-	
-	a = b;
-	b = tmp;
-}
-
-template<typename T>
-T	min(T &a, T &b)	{
-	return(a < b ? a: b);
-}
-
-template<typename T>
-T	max(T &a, T &b)	{
-	return(a > b ? a: b);
+template<typename T, typename T3>
+void	iter(T a, size_t b, T3 c)	{
+	for(size_t i = 0; i < b; i++)	{
+		c(a[i]);
+	}
 }
